de7a0c02a77cc96728ff75383f4ce2c3431d8d8e
ken <pontaiponji@gmail.com>
2026-01-19 16:37:01 +0900

    add: guruguru

--- layout/StoryCreator/index.tsx
+++ layout/StoryCreator/index.tsx
@@ -169,7 +169,7 @@ const StoryCreator = () => {
       <div className="flex-1 mt-12 overflow-auto">
         <div className="p-4 flex flex-col items-center">
           <div className="max-w-100 w-full bg-white flex flex-col items-center p-4 pt-8 space-y-4 rounded-xl">
-            {currentPhase === "selectWords" && wordCardOptions && (
+            {currentPhase === "selectWords" && wordCardOptions && !isSubmitting && (
               <WordListPicker
                 key="wordlist"
                 wordsList={wordCardOptions}
@@ -178,7 +178,7 @@ const StoryCreator = () => {
                 }}
               />
             )}
-            {currentPhase == "createStory" && (
+            {currentPhase == "createStory" && !isSubmitting && (
               <CreateStory
                 words={selectedWords}
                 usedWords={new Set(usedWords.map((w) => w.toLowerCase()))}
@@ -192,7 +192,7 @@ const StoryCreator = () => {
                 }}
               />
             )}
-            {currentPhase === "setTitleThumbnail" && (
+            {currentPhase === "setTitleThumbnail" && !isSubmitting && (
               <TitleThumbnailSetter
                 title={title}
                 setTitle={setTitle}
@@ -200,6 +200,29 @@ const StoryCreator = () => {
                 thumbnailId={thumbnailId}
                 setThumbnailId={setThumbnailId}
               />
+            )}
+            {isSubmitting && (
+              <div className="flex items-center text-[#93C400]">
+                <svg
+                  className="animate-spin mr-3 h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12" cy="12" r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                <span className="text-lg font-bold">ローディング中...</span>
+              </div>
             )}
           </div>
         </div>
